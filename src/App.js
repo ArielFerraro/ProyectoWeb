33e75ab2b549fcd81d513dc5243aedb8f433ec3d
ArielFerraro <ariel.ferraro74@hotmail.com>
2023-02-06 17:15:11 -0300

    primeros pasos del proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+
+export default App
